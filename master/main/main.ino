05dfc37bccda7c16695f07b704a485bd8a10d601
Pyry Lahtinen <pyry@pyry.info>
2025-07-10 18:13:11 +0300

    sync clocks

--- master/main/main.ino
+++ master/main/main.ino
@@ -5,9 +5,61 @@
 
 #define LED_PIN 4 // led
 
-uint8_t slaveMac[] = {0xC8, 0xF0, 0x9E, 0x4D, 0xB5, 0xA8}; // C8:F0:9E:4D:B5:A8
+uint8_t slaveMac[] = {0xC8, 0xF0, 0x9E, 0x4D, 0x64, 0x0C};
 
 bool is_slave_connected = false;
+bool is_waiting_for_response = false;
+String received_response = "";
+
+unsigned long slave_clock_offset;
+
+String send_message(String message) {
+  if (!is_slave_connected) {
+    Serial.println("Trying to send message without connected slave");
+    return "";
+  }
+
+  bool send_success = esp_now_send(slaveMac, (uint8_t*)message.c_str(), message.length());
+  if (send_success != ESP_OK) {
+    Serial.println("Message send failed: " + String(send_success));
+    return "";
+  }
+  is_waiting_for_response = true;
+
+  while (is_waiting_for_response) {
+    delay(1);
+  }
+
+  String response = received_response;
+  received_response = "";
+
+  return response;
+}
+
+void onReceive(const esp_now_recv_info* info, const unsigned char* data, int len) {
+  String msg = "";
+  for (int i = 0; i < len; i++) {
+    msg += (char)data[i];
+  }
+
+  received_response = msg;
+  is_waiting_for_response = false;
+}
+
+
+bool send_ping_pong() {
+  Serial.println("Sending ping-pong");
+  String random_payload = "12345678";
+  String response = send_message("pin" + random_payload);
+  if (response == ("pon" + random_payload)) {
+    return true;
+  } else {
+    Serial.println("Ping-pong epäonnistui: \"" + response + "\"");
+    return false;
+  }
+}
+
+
 
 void blink(int count) {
   for (int i=0; i<count; i++) {
@@ -22,13 +74,22 @@ void blink(int count) {
   }
 }
 
-bool connect_slave() {
+void connect_slave() {
   WiFi.mode(WIFI_STA);
+
+  // Serial.print("Waiting mac address...");
+  // while (WiFi.macAddress() == "00:00:00:00:00:00") {
+  //   delay(100);
+  //   Serial.print(".");
+  // }
+  // Serial.println("\nMAC address: " + WiFi.macAddress());
 
   if (esp_now_init() != ESP_OK) {
     Serial.println("ESP-NOW init failed");
-    return false;
+    return;
   }
+
+  esp_now_register_recv_cb(onReceive);
 
   esp_now_peer_info_t peer;
   memset(&peer, 0, sizeof(peer));
@@ -37,21 +98,67 @@ bool connect_slave() {
   peer.encrypt = false;
   peer.ifidx = WIFI_IF_STA;
   esp_err_t result = esp_now_add_peer(&peer);
-  if (result == ESP_OK) {
-    Serial.println("Slave lisätty onnistuneesti");
-  } else {
+  if (result != ESP_OK) {
     Serial.printf("Slaven lisäys epäonnistui: %d\n", result);
-    return false;
+    return;
+  }
+  is_slave_connected = true;
+
+  if (!send_ping_pong()) {
+    is_slave_connected = false;
+  }
+}
+
+bool sync_clocks() {
+  unsigned long avg_offset;
+  unsigned long min_offset = 9999999999999;
+  unsigned long max_offset = 0;
+
+  unsigned long avg_rtt = 0;
+
+  for (int i=0; i<5; i++) {
+    unsigned long t0 = millis();
+    String response = send_message("syn");
+    unsigned long t2 = millis();
+
+    if (response.length() != 13 || response.substring(0, 3) != "syr") {
+      Serial.println("Sync failed: invalid response from slave, invalid format. \"" + response + "\"");
+      return false;
+    }
+    char* end_ptr;
+    unsigned long t1 = strtoul(response.substring(3, 13).c_str(), &end_ptr, 10);
+    if (*end_ptr != '\0') { // conversion failed
+      Serial.println("Sync failed: invalid response from slave, conversion failed. \"" + response + "\"");
+      return false;
+    }
+
+    unsigned long offset = t1 - (t0 + t2) / 2;
+    avg_offset += offset;
+    if (offset < min_offset) {
+      min_offset = offset;
+    }
+    if (offset > max_offset) {
+      max_offset = offset;
+    }
+
+    avg_rtt += t2 - t0;
+
+    delay(100);
   }
 
-  // TODO: ping-pong handshake
+  avg_rtt /= 5;
+
+  Serial.printf("Sync done. Average RTT %d ms\n", avg_rtt);
+
+  if (max_offset - min_offset > 20) {
+    Serial.printf("Warning! Great variance in clock offsets: %d - %d ms\n", min_offset, max_offset);
+  }
+
+  slave_clock_offset = avg_offset / 5;
 
   return true;
 }
 
-bool send_message(String message) {
-  return esp_now_send(slaveMac, (uint8_t*)message.c_str(), message.length()) == ESP_OK;
-}
 
 void setup() {
   pinMode(LED_PIN, OUTPUT);
@@ -60,19 +167,27 @@ void setup() {
 
   blink(1);
 
-  is_slave_connected = connect_slave();
+  connect_slave();
 
+  bool sync_success = false;
   if (is_slave_connected) {
+    sync_success = sync_clocks();
+  }
+
+  if (is_slave_connected && sync_success) {
     blink(2);
   } else {
     blink(7);
+    return;
   }
+
+  Serial.println("Setup completed successfully");
 }
 
 void loop() {
-  if (is_slave_connected) {
-    send_message("Moikka slave");
-  }
+  // if (is_slave_connected) {
+  //   send_message("Moikka slave");
+  // }
 
   delay(10000);
 }
--- slave/main/main.ino
+++ slave/main/main.ino
@@ -1,9 +1,12 @@
 #include <esp_now.h>
 #include <WiFi.h>
+#include <inttypes.h> // todo
 
 // SLAVE
 
 #define LED_PIN 4 // led
+
+uint8_t masterMac[] = {0xC8, 0xF0, 0x9E, 0x4D, 0xB5, 0xA8};
 
 void blink(int count) {
   for (int i=0; i<count; i++) {
@@ -18,30 +21,69 @@ void blink(int count) {
   }
 }
 
+void send_response(String message) {
+  esp_now_send(masterMac, (uint8_t*)message.c_str(), message.length());
+}
+
 void onReceive(const esp_now_recv_info* info, const unsigned char* data, int len) {
   String msg = "";
   for (int i = 0; i < len; i++) {
     msg += (char)data[i];
   }
-  Serial.println("Vastaanotettiin: " + msg);
+  
+  String message_type = msg.substring(0, 3);
+
+  if (message_type == "pin") {
+    handle_ping_pong(msg);
+  } else if (message_type == "syn") {
+    handle_clock_sync();
+  } else {
+    Serial.println("Received unexpected message: " + msg);
+  }
+}
+
+void handle_ping_pong(String message) {
+  Serial.println("Received ping-pong " + message);
+  String random_payload = message.substring(3, 11);
+  send_response("pon" + random_payload);
+}
+
+void handle_clock_sync() {
+  Serial.println("Received clock sync");
+  unsigned long time_now = millis();
+  char time_string[11];
+  sprintf(time_string, "%010lu", time_now);
+  String message = "syr" + String(time_string);
+  send_response(message);
 }
 
 void setup_wifi() {
   WiFi.mode(WIFI_STA);
 
   // Serial.print("Waiting mac address...");
-  // while(WiFi.macAddress() == "00:00:00:00:00:00") {
+  // while (WiFi.macAddress() == "00:00:00:00:00:00") {
   //   delay(100);
   //   Serial.print(".");
   // }
   // Serial.println("\nMAC address: " + WiFi.macAddress());
 
-  esp_now_init();
-  // if (esp_now_init() != ESP_OK) {
-  //   Serial.println("ESP-NOW init failed");
-  //   return false;
-  // }
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("ESP-NOW init failed");
+    return;
+  }
+
   esp_now_register_recv_cb(onReceive);
+
+  esp_now_peer_info_t peer;
+  memset(&peer, 0, sizeof(peer));
+  memcpy(peer.peer_addr, masterMac, 6);
+  peer.channel = 0;
+  peer.encrypt = false;
+  peer.ifidx = WIFI_IF_STA;
+  esp_err_t result = esp_now_add_peer(&peer);
+  if (result != ESP_OK) {
+    Serial.printf("Masterin lisäys epäonnistui: %d\n", result);
+  }
 }
 
 void setup() {
