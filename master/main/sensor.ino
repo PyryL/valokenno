ce28fea9ca3a08b7b16c448be0bfe6081d4bc97b
Pyry Lahtinen <pyry@pyry.info>
2025-07-17 17:58:14 +0300

    improve sensor handling

--- master/main/sensor.ino
+++ master/main/sensor.ino
@@ -1,7 +1,8 @@
 #include <Wire.h>
 #include <VL53L0X.h>
 
-#define MOTION_DETECTION_THRESHOLD 1200 // millimeters
+#define MOTION_DETECTION_MIN_THRESHOLD 100 // millimeters
+#define MOTION_DETECTION_MAX_THRESHOLD 1000 // millimeters
 
 VL53L0X sensor;
 
@@ -15,12 +16,25 @@ void set_led(bool is_on) {
 
 
 void setup_sensor() {
-  Wire.begin(14, 15);
+  bool i2c_success = Wire.begin(14, 15);
+  if (!i2c_success) {
+    Serial.println("I2C setup failed");
+    return;
+  }
 
-  sensor.init();
+  bool sensor_success = sensor.init();
+  if (!sensor_success) {
+    Serial.println("Sensor init failed"); // happens when sensor not wired up
+    return;
+  }
+
   sensor.setTimeout(500);
 
-  sensor.setMeasurementTimingBudget(20000);
+  bool budget_success = sensor.setMeasurementTimingBudget(20000);
+  if (!budget_success) {
+    Serial.println("Measurement timing budget setting failed");
+    return;
+  }
 
   sensor.startContinuous();
 }
@@ -29,7 +43,7 @@ void loop_sensor() {
   uint16_t distance_reading = sensor.readRangeContinuousMillimeters();
   unsigned long timestamp = millis();
 
-  if (distance_reading <= MOTION_DETECTION_THRESHOLD) {
+  if (distance_reading > MOTION_DETECTION_MIN_THRESHOLD && distance_reading <= MOTION_DETECTION_MAX_THRESHOLD) {
     if (!did_detect_motion_last_time) {
       motion_timestamps.push_back(timestamp);
       did_detect_motion_last_time = true;
